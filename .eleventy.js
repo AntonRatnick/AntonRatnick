700cbb220ee400d3266e64101e9b018947ec1e80
AntonRatnick <AntonRatnick@gmail.com>
2020-08-15 15:39:17 +0300

    chore: add minification

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,26 @@
-const postcss = require("postcss");
-const precss = require("precss");
-const autoprefixer = require("autoprefixer");
+const postcss = require('postcss');
+const htmlmin = require('html-minifier');
+const precss = require('precss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
 const dateFilter = require('./src/filters/date.js');
 
 module.exports = (config) => {
-  config.addNunjucksAsyncFilter("postCSS", (code, callback) =>
-    postcss([precss, autoprefixer])
+  config.addTransform('htmlmin', function (content, outputPath) {
+    if (outputPath.endsWith('.html')) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
+
+  config.addNunjucksAsyncFilter('postCSS', (code, callback) =>
+    postcss([precss, autoprefixer,  cssnano])
       .process(code)
       .then((res) => callback(null, res))
   );
@@ -13,11 +28,11 @@ module.exports = (config) => {
   config.addFilter('date', dateFilter);
 
   return {
-    htmlTemplateEngine: "njk",
+    htmlTemplateEngine: 'njk',
     dir: {
       input: 'src',
-      output: 'dist'
+      output: 'dist',
     },
-    passthroughFileCopy: true
+    passthroughFileCopy: true,
   };
 };
